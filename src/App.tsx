a853b13a040fb00f46c727bf0759ed9b1e7e11e6
Alen <alen@formant.io>
2023-10-09 13:50:24 -0500

    add console

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
+import { useEffect } from "react";
 import "./App.css";
 import { config } from "./config";
 
 function App() {
+  useEffect(() => {
+    console.log(import.meta.env.VITE_STAGE);
+  }, []);
   return (
     <>
       {config.outputFolder || "NONe"}
